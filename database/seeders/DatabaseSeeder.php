60079c8f25eb598f3eb4bf47dc22330543066b37
Raul Chauvin <raullduch@gmail.com>
2022-02-09 18:12:31 -0500

    Configuracion inicial de archivos del proyecto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use \App\Models\User;
+use \App\Models\Note;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,6 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Raul Chauvin',
+            'email' => 'raul@buhoo.ec',
+            'password' => bcrypt('12345678')
+        ]);
+        Note::factory(30)->create();
     }
 }
